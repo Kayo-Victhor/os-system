9c48436abd54b25a1c76fbecaeb4ca98e636eb09
Moura-linuq <xaolin997@gmail.com>
2026-08-11 10:11:00 -0300

    first commit

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log(`API rodando em http://localhost:${PORT}`);
+});
